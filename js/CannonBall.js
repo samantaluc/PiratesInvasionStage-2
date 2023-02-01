eaa54c97d95ef202e01495e817e2cb0900d00c78
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-02-01 10:57:59 -0300

    Update CannonBall.js

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -1,24 +1,22 @@
 class CannonBall {
   constructor(x, y) {
     var options = {
-      restitution: 0.8,
-      friction: 1.0,
-      density: 1.0,
-      isStatic: true
+      restitution: 0.8, //elasticidade
+      friction: 1.0, //fricção do ar
+      density: 1.0, // densidade = peso
+      isStatic: true // estatica
     };
-    this.r = 40;
-
-    this.body = Bodies.circle(x, y, this.r, options);
-
-    this.image = loadImage("./assets/cannonball.png");
-    World.add(world, this.body);
+    this.r = 40; // medida do raio
+    this.body = Bodies.circle(x, y, this.r, options); //crie um corpo circular
+    this.image = loadImage("./assets/cannonball.png"); //carregue a imagem
+    World.add(world, this.body); // adição ao mundo
   }
 
   shoot() {
-    var velocity = p5.Vector.fromAngle(cannon.angle);
-    velocity.mult(20);
-    Matter.Body.setStatic(this.body, false);
-    Matter.Body.setVelocity(this.body, { x: velocity.x, y: velocity.y });
+    var velocity = p5.Vector.fromAngle(cannon.angle);     //defina o ângulo da bala para o canhão
+    velocity.mult(20); // valor de velocidade aplicada inicialmente
+    Matter.Body.setStatic(this.body, false);    //defina valor estático para o corpo
+    Matter.Body.setVelocity(this.body, { x: velocity.x, y: velocity.y });     //defina a velocidade da bala para fazer a bala se mover
   }
 
   display() {
@@ -28,9 +26,8 @@ class CannonBall {
     translate(pos.x, pos.y);
     rotate(angle);
     imageMode(CENTER);
-    image(this.image, 0, 0, this.r, this.r);
+    image(this.image, 0, 0, this.r, this.r); // mostra a imagem
     pop();
-
     }
   }
 
